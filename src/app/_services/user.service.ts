d1976684807d9d2e9504427eb5a30d9107b4c1c3
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-06 14:14:19 +0700

    Update user.service.ts

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -27,6 +27,6 @@ export class UserService {
     return this.http.put<{ logToken: string }>(`${apiURL}/login/forget-password`, data);
   }
   logout() {
-    return this.http.patch(`${apiURL}/logout`, this.auth.getToken);
+    return this.http.delete(`${apiURL}/logout`);
   }
 }
