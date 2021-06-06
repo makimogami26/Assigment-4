8b696b575f86c3593f6cc55725e694f8b958ec18
makimogami26 <dspryd26@gmail.com>
2021-06-06 18:07:00 +0700

    add Produk+get

--- src/app/_components/shared/product-list/product-list.component.ts
+++ src/app/_components/shared/product-list/product-list.component.ts
@@ -1,15 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/_models/Product';
+import { AuthService } from 'src/app/_services/auth.service';
+import { InventoryService } from 'src/app/_services/inventory.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-product: any;
-  constructor() { }
+  Product!: Product[];
+  constructor(
+    private http: HttpClient,
+    private inventoryService: InventoryService,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.inventoryService.getProduct().subscribe((Product) => {
+      this.Product = Product;
+    });
   }
-
 }
